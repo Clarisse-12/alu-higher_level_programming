c7a71997bd00dfcc5826cd2f26ec9143399c5680
Clarisse-12 <c.mukayiran@alustudent.com>
2024-10-23 16:11:52 +0200

    task 3

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-print("{}".format("".join("{:c}".format(i) for i in range(97, 123) if i not in (101, 113))), end="")
+print("{}".format("".join(
+    "{:c}".format(i) for i in range(97, 123) if i not in (101, 113)
+)), end="")
