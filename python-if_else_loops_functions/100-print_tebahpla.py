6f08622b963ceee6b6e4ccb085a16c3edc16a211
Clarisse-12 <c.mukayiran@alustudent.com>
2024-10-24 11:56:52 +0200

    task 14

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def print_reverse_alternate():
-    print("{}".format("".join([chr(ord('z') - i) if i % 2 == 0 else chr(ord('Z') - i) for i in range(26)])), end="")
+    for i in range(26):
+        print("{:c}".format(122 - i - (i % 2) * 32), end="")
