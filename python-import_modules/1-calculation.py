85c45d17cc1b5736dff3aa52469f12109f4514ee
Clarisse-12 <c.mukayiran@alustudent.com>
2024-10-24 19:36:42 +0200

    task 1

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
-
     a = 10
     b = 5
-
-    print(str(a) + " + " + str(b) + " = " + str(add(a, b)))
+print(str(a) + " + " + str(b) + " = " + str(add(a, b)))
     print(str(a) + " - " + str(b) + " = " + str(sub(a, b)))
     print(str(a) + " * " + str(b) + " = " + str(mul(a, b)))
     print(str(a) + " / " + str(b) + " = " + str(div(a, b)))
