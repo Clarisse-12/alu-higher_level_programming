b13c7bfae99100a918da9a32239e140fbcd976a7
Clarisse-12 <c.mukayiran@alustudent.com>
2024-10-24 08:22:33 +0200

    task 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,5 @@
 for i in range(0, 10):
     for j in range(i + 1, 10):
         if i != 8 or j != 9:
-            print(f"{i}{j}, ", end="")
-print(f"{8}{9}")
+            print(str(i) + str(j) + ", ", end="")
+print(str(8) + str(9))
