561c6ff8695db059443224e2705b2f2df2e15a1e
Clarisse-12 <c.mukayiran@alustudent.com>
2024-10-24 12:39:17 +0200

    task16

--- python-if_else_loops_functions/102-magic_calculation.py
+++ python-if_else_loops_functions/102-magic_calculation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def magic_calculation(a, b, c):
     if a < b:
-        return 3
-    else:
-        return a + b + c
+        return c
+    elif c > b:
+        return a + b
+    return (a * b) - c
