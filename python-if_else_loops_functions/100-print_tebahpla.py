8ea7e12cee0415c8350ff4b43b9fa03b878721bd
Clarisse-12 <c.mukayiran@alustudent.com>
2024-10-24 12:09:05 +0200

    task 14

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def print_alphabet():
     for i in range(26):
-        print("{:s}".format(chr(122 - i) if i % 2 == 0 else chr(90 - i)), end="")
+        print("{:s}".format(
+            chr(122 - i) if i % 2 == 0 else chr(90 - i)),
+            end=""
+        )
 
 print_alphabet()
 
