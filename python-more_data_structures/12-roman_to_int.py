5035bdd2c90d41fcd1750f5eb160da1ee73cf314
Clarisse-12 <c.mukayiran@alustudent.com>
2024-10-25 12:28:21 +0200

    task12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -6,7 +6,8 @@ def roman_to_int(roman_string):
     roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     total = 0
     prev_value = 0
-for char in reversed(roman_string):
+
+    for char in reversed(roman_string):
         value = roman_numerals.get(char, 0)
         if value < prev_value:
             total -= value
