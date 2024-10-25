aa2c87e97950e9fbadd2e44aca81e49491f308e1
Clarisse-12 <c.mukayiran@alustudent.com>
2024-10-25 09:07:17 +0200

    task6

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+    
     from calculator_1 import add, sub, mul, div
 if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
@@ -19,4 +20,5 @@ if operator == '+':
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
+    
     print(f"{a} {operator} {b} = {result}")
