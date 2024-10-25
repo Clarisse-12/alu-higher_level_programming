f229a1f290b07a218753dca387441030cd90a46d
Clarisse-12 <c.mukayiran@alustudent.com>
2024-10-25 09:11:32 +0200

    task6

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -6,9 +6,11 @@ if __name__ == "__main__":
 if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
+
 a = int(sys.argv[1])
     operator = sys.argv[2]
     b = int(sys.argv[3])
+
 if operator == '+':
         result = add(a, b)
     elif operator == '-':
