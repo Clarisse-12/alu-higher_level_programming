076cae17d968917be06561e8dc6a712e1d0d09b5
Clarisse-12 <c.mukayiran@alustudent.com>
2024-10-25 12:20:03 +0200

    task 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-def multiply_by_2(a_dictionary):
-    return {key: value * 2 for key, value in a_dictionary.items()}
+def best_score(a_dictionary):
+    if a_dictionary:
+        return max(a_dictionary, key=a_dictionary.get)
+    return None
