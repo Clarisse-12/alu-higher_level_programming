2aa7d004d691cd1e9ea3a41aa3d3620088d82899
Clarisse-12 <c.mukayiran@alustudent.com>
2024-10-25 09:03:59 +0200

    task6

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -2,19 +2,13 @@
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
-
-
-    if len(sys.argv) != 4:
+if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-
-
-    a = int(sys.argv[1])
+a = int(sys.argv[1])
     operator = sys.argv[2]
     b = int(sys.argv[3])
-
-
-    if operator == '+':
+if operator == '+':
         result = add(a, b)
     elif operator == '-':
         result = sub(a, b)
