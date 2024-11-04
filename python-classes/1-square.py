864e4f920ec087b25601da630b34f9b99d71b0f7
Clarisse-12 <c.mukayiran@alustudent.com>
2024-11-04 09:00:31 +0200

    task 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -2,14 +2,14 @@
 """
 This module defines a Square class with a private instance attribute for size.
 The Square class represents a square shape with a specified size attribute,
-which is kept private to ensure controlled access and modification in later tasks.
+which is kept private to ensure controlled access and modification.
 """
 
 
 class Square:
     """
     Class that defines a square with a private instance attribute for size.
-    
+
     The size of a square is kept private to allow controlled access in the
     future, as the size attribute will be important for calculations such as
     area and perimeter.
@@ -21,9 +21,8 @@ class Square:
 
         Args:
             size: The size of one side of the square.
-        
-        This size attribute is private to ensure controlled access and 
+
+        This size attribute is private to ensure controlled access and
         modification, which will be crucial for size-dependent calculations.
         """
         self.__size = size
-
