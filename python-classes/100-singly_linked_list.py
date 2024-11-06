6283a3b5603874170faf452abfadf8d0ad2e947e
Clarisse-12 <c.mukayiran@alustudent.com>
2024-11-06 13:54:21 +0200

    task 7

--- python-classes/100-singly_linked_list.py
+++ python-classes/100-singly_linked_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""This module contains a class that defines a square.
+""" 
+This module contains a class that defines a square.
 In the Square class we initialize each object by the
 __init__ method with a private instance variable called
 __size that takes the size variable's value passed as
