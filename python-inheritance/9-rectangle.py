f59a5b463972494ca7d6335425370a859bb12dc7
Clarisse-12 <c.mukayiran@alustudent.com>
2024-11-08 07:35:20 +0200

    task 9

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-"""
-Rectangle class Module
-"""
-
-
+"""Rectangle class Module"""
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
@@ -15,3 +11,11 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
+
+    def area(self):
+        """Method that returns area of rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Returns a string"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
