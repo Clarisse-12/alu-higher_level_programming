d88c99156ef459466eab1f4bb61f2a126736304f
Clarisse-12 <c.mukayiran@alustudent.com>
2024-11-15 12:01:00 +0200

    task 0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,17 +1,26 @@
 
 -- Script that lists all privileges of the MySQL users
 -- Query to list all privileges (GRANT) of the MySQL uses
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
-
 DELIMITER //
 
 CREATE PROCEDURE RevokeAllPrivileges(IN username VARCHAR(100), IN host VARCHAR(100))
 BEGIN
     DECLARE done INT DEFAULT 0;
     DECLARE grant_statement TEXT;
-    DECLARE cur CURSOR FOR SHOW GRANTS FOR username@host;
+    DECLARE cur CURSOR FOR SELECT grant_query FROM mysql.temp_grants;
     DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+
+    CREATE TEMPORARY TABLE IF NOT EXISTS mysql.temp_grants (grant_query TEXT);
+
+    SET @show_grants_query = CONCAT("SHOW GRANTS FOR '", username, "'@'", host, "'");
+    PREPARE stmt FROM @show_grants_query;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
+
+    INSERT INTO mysql.temp_grants (grant_query)
+    SELECT DISTINCT CONCAT('GRANT ', PRIVILEGE_TYPE, ' ON *.* TO ''', username, '''@''', host, '''')
+    FROM information_schema.user_privileges
+    WHERE GRANTEE = CONCAT("'", username, "'@", host);
 
     OPEN cur;
     read_loop: LOOP
@@ -28,6 +37,8 @@ BEGIN
         DEALLOCATE PREPARE stmt;
     END LOOP;
     CLOSE cur;
+
+    DROP TEMPORARY TABLE IF EXISTS mysql.temp_grants;
 END//
 
 DELIMITER ;
