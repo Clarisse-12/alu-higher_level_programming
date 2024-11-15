a93e24ae9333f079c0c4f2676b12d808d2a967a1
Clarisse-12 <c.mukayiran@alustudent.com>
2024-11-15 12:21:35 +0200

    task 0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,9 @@
 -- Script that lists all privileges of the MySQL users
 -- Query to list all privileges (GRANT) of the MySQL users
+SELECT USER, HOST 
+FROM mysql.user 
+WHERE CONCAT(USER, '@', HOST) IN ('user_0d_1@localhost', 'user_0d_2@localhost');
+
 SELECT 
     GRANTEE, PRIVILEGE_TYPE, IS_GRANTABLE 
 FROM 
