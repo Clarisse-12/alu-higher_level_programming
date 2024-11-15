c0639b817c51fff80446253f6d50dcac460898bf
Clarisse-12 <c.mukayiran@alustudent.com>
2024-11-15 11:29:57 +0200

    task 0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,3 @@
--- Script that lists all privileges of the MySQL users
--- Query to list all privileges (GRANT) of the MySQL users
-
+-- Script that lists all privileges of the MySql
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
