883b1d8e0f72c98a09e63b1fbfb6d3c2c692933f
Clarisse-12 <c.mukayiran@alustudent.com>
2024-11-15 11:51:10 +0200

    task 0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,6 @@
+
+-- Script that lists all privileges of the MySQL users
+-- Query to list all privileges (GRANT) of the MySQL users
 REVOKE SELECT, INSERT, UPDATE ON *.* FROM 'user_0d_1'@'localhost';
 REVOKE CREATE, DROP ON db1.* FROM 'user_0d_1'@'localhost';
 
