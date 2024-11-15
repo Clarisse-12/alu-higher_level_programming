cac1bb593e4fff95d822eec06bcb82da22f1e97a
Clarisse-12 <c.mukayiran@alustudent.com>
2024-11-15 12:32:26 +0200

    task 0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,12 +1,31 @@
 -- Script that lists all privileges of the MySQL users
 -- Query to list all privileges (GRANT) of the MySQL users
-SELECT USER, HOST 
-FROM mysql.user 
-WHERE CONCAT(USER, '@', HOST) IN ('user_0d_1@localhost', 'user_0d_2@localhost');
+import mysql.connector
 
-SELECT 
-    GRANTEE, PRIVILEGE_TYPE, IS_GRANTABLE 
-FROM 
-    INFORMATION_SCHEMA.USER_PRIVILEGES
-WHERE 
-    GRANTEE IN ('\'user_0d_1\'@\'localhost\'', '\'user_0d_2\'@\'localhost\'');
+def list_user_privileges(users):
+    try:
+        connection = mysql.connector.connect(
+            host='localhost',
+            user='your_username',
+            password='your_password'
+        )
+        
+        cursor = connection.cursor()
+        
+        for user in users:
+            cursor.execute(f"SHOW GRANTS FOR '{user}'@'localhost';")
+            grants = cursor.fetchall()
+            print(f"Privileges for {user}:")
+            for grant in grants:
+                print(grant[0])
+            print("\n")
+            
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+
+users_to_check = ['user_0d_1', 'user_0d_2']
+list_user_privileges(users_to_check)
