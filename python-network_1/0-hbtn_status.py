a84b9b823ba99ffb259b5c7e3d39227cae744502
Clarisse-12 <c.mukayiran@alustudent.com>
2024-11-19 16:08:53 +0200

    task 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+        print("\t- content: {Custom status}".format(body))
+        print("\t- utf8 content: {Custom status}".format(body.decode("utf-8")))
