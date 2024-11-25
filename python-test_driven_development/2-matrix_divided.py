f634360deba5d37d28aee1e265e485860d54eb32
Clarisse-12 <c.mukayiran@alustudent.com>
2024-11-25 13:45:01 +0200

    task 2

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,88 +1,46 @@
 #!/usr/bin/python3
 """
-Module composed by a function that multiplies 2 matrices
+    matrix division module
 """
 
 
-def matrix_mul(m_a, m_b):
-    """ Function that multiplies 2 matrices
+def matrix_divided(matrix, div):
+    """ Takes a matrix and divides the values by 'div'.
+
     Args:
-        m_a: matrix a
-        m_b: matrix b
-    Returns:
-        result of the multiplication
-    Raises:
-        TypeError: if m_a or m_b aren't a list
-        TypeError: if m_a or m_b aren't a list of a lists
-        ValueError: if m_a or m_b are empty
-        TypeError: if the lists of m_a or m_b don't have integers or floats
-        TypeError: if the rows of m_a or m_b don't have the same size
-        ValueError: if m_a and m_b can't be multiplied
+        matrix (:obj:'list' of :obj:'list'): lists of lists of integers/floats.
+        div (int or float): The divisor.
     """
+    row_len = -1
+    if type(div) is not int and type(div) is not float:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
 
-    if not isinstance(m_a, list):
-        raise TypeError("m_a must be a list")
+    if type(matrix) != list or len(matrix) == 0:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/"
+                        "floats")
 
-    if not isinstance(m_b, list):
-        raise TypeError("m_b must be a list")
-
-    for elems in m_a:
-        if not isinstance(elems, list):
-            raise TypeError("m_a must be a list of lists")
-
-    for elems in m_b:
-        if not isinstance(elems, list):
-            raise TypeError("m_b must be a list of lists")
-
-    if len(m_a) == 0 or (len(m_a) == 1 and len(m_a[0]) == 0):
-        raise ValueError("m_a can't be empty")
-
-    if len(m_b) == 0 or (len(m_b) == 1 and len(m_b[0]) == 0):
-        raise ValueError("m_b can't be empty")
-
-    for lists in m_a:
-        for elems in lists:
-            if not type(elems) in (int, float):
-                raise TypeError("m_a should contain only integers or floats")
-
-    for lists in m_b:
-        for elems in lists:
-            if not type(elems) in (int, float):
-                raise TypeError("m_b should contain only integers or floats")
-
-    length = 0
-
-    for elems in m_a:
-        if length != 0 and length != len(elems):
-            raise TypeError("each row of m_a must be of the same size")
-        length = len(elems)
-
-    length = 0
-
-    for elems in m_b:
-        if length != 0 and length != len(elems):
-            raise TypeError("each row of m_b must be of the same size")
-        length = len(elems)
-
-    if len(m_a[0]) != len(m_b):
-        raise ValueError("m_a and m_b can't be multiplied")
-
-    r1 = []
-    i1 = 0
-
-    for a in m_a:
-        r2 = []
-        i2 = 0
-        num = 0
-        while (i2 < len(m_b[0])):
-            num += a[i1] * m_b[i1][i2]
-            if i1 == len(m_b) - 1:
-                i1 = 0
-                i2 += 1
-                r2.append(num)
-                num = 0
+    new_matrix = []
+    for row in matrix:
+        if type(row) is not list:
+            raise TypeError("matrix must be a matrix (list of lists) of "
+                            "integers/floats")
+        if row_len == -1:
+            row_len = len(row)
+            if row_len == 0:
+                raise TypeError("matrix must be a matrix (list of lists) of "
+                                "integers/floats")
+        else:
+            if row_len is not len(row):
+                raise TypeError("Each row of the matrix must have the same "
+                                "size")
+        new_row = []
+        for ele in row:
+            if type(ele) is int or type(ele) is float:
+                new_row.append(round(ele / div, 2))
             else:
-                i1 += 1
-        r1.append(r2)
-
-    return r1
+                raise TypeError("matrix must be a matrix (list of lists) of "
+                                "integers/floats")
+        new_matrix.append(new_row)
+    return new_matrix
